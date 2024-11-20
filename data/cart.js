1c5d48726deaeb33b129b00a446832af4003c25a
Jshuashrewd <joshadeyanju23@gmail.com>
2024-11-20 20:05:58 +0100

    made changes

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
-const cart = [];
+export const cart = [];
+
+export function addToCart(productId){
+  let marchingItem;
+
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        marchingItem = cartItem;
+      }
+    });
+
+    if (marchingItem) {
+      marchingItem.quantity++;
+    } else {
+      cart.push({
+      productId: productId,
+      quantity: 1
+    })
+    }
+}
