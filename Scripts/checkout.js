a108215a92e36119a3bed14209caff2b1e9b194b
Jshuashrewd <joshadeyanju23@gmail.com>
2024-11-25 17:53:27 +0100

    make delivery options interactive

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart,removeFromCart } from "../data/cart.js";
+import { cart,removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@1.11.13/+esm'
@@ -91,7 +91,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId
 
     html +=`
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id = "${deliveryOption.id}">
       <input type="radio" ${isChecked ? 'checked' : ''}
         class="delivery-option-input"
         name="delivery-option-${matchingProduct.id}">
@@ -120,3 +120,10 @@ document.querySelectorAll('.js-delete-link').forEach( (link) => {
     container.remove();
   });
 });
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+  element.addEventListener('click', () => {
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
+  })
+})
--- data/cart.js
+++ data/cart.js
@@ -17,16 +17,16 @@ function saveToStorage() {
 }
 
 export function addToCart(productId){
-  let marchingItem;
+  let matchingItem;
 
     cart.forEach((cartItem) => {
       if (productId === cartItem.productId) {
-        marchingItem = cartItem;
+        matchingItem = cartItem;
       }
     });
 
-    if (marchingItem) {
-      marchingItem.quantity++;
+    if (matchingItem) {
+      matchingItem.quantity++;
     } else {
       cart.push({
       productId: productId,
@@ -47,6 +47,20 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption (productId, deliveryOptionId){
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
