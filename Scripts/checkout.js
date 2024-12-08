8206bbc25280154f1aaea7bfd120c1ccaef53f0c
Jshuashrewd <joshadeyanju23@gmail.com>
2024-12-08 18:10:31 +0100

    convert loadProducts from XHR to fetch

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,14 +1,14 @@
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 // import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve();
-  });
-}).then(() => {
+Promise.all([
+  loadProductsFetch(),
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 })
